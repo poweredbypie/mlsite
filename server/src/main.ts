adb2d16fba4d1b28dce36378a733355e908f493d
Ben Barber <benbarber121@gmail.com>
2022-12-12 21:35:56 -0500

    fix tsconfig bullshit

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,8 @@
-import express, { Request, Response } from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import path from 'path';
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import path from "path";
 
 const app = express();
 const port = 3000;
@@ -13,5 +13,5 @@ app.use("/", express.static(path.resolve(__dirname, "../client")));
 mongoose.connect(process.env.MONGODB_URI as string);
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`)
+  console.log(`App listening at http://localhost:${port}`);
 });
